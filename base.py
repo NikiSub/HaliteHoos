40043729c4ce0042ae5afdc5dba4ad2af248e0f2
Ashwin Pathi <AshwinPathi@users.noreply.github.com>
2020-05-15 21:10:12 -0400

    added naive algo

--- base.py
+++ base.py
@@ -2,6 +2,7 @@ import random
 from kaggle_environments.envs.halite.halite import get_to_pos
 import logging
 import numpy as np
+import time
 logging.basicConfig(filename='stdout.log',level=logging.DEBUG)
 
 DIRS = ["NORTH", "SOUTH", "EAST", "WEST", None]
@@ -23,6 +24,8 @@ class HaliteBoard():
 	def __init__(self, obs):
 		self.height = self.width = BOARD_DIMS
 
+		_, self.shipyards, self.ships = obs.players[obs.player]
+
 		self.agent_board_1d = np.array(list(range(self.width*self.height)))
 		self.agent_board_2d = np.array(list(range(self.width*self.height))).reshape(self.width, self.height)
 
@@ -36,10 +39,20 @@ class HaliteBoard():
 		halite_coords = list(zip(hx,hy))
 		return halite_coords
 
-
 	# returns positions of ally shipyards
 	def get_shipyard_locations(self):
+		return [int_to_coords(shipyard) for uid, shipyard in self.shipyards.items()]
 
+	def get_closest_shipyard(self, curr_pos):
+		shipyard_coords = self.get_shipyard_locations()
+		distances = {}
+		for i in shipyard_coords:
+			# find euclidean distance, doesn't take into account wrap around
+			dist = np.sqrt((i[0] - curr_pos[0])**2 + (i[1] - curr_pos[1])**2)
+			distances[i] = dist
+		# from the dict get the closest set of coords     
+		closest_xy =  min(distances, key=distances.get)
+		return closest_xy
 
 	# returns the position of the closest halite deposit above a certain threshold
 	def get_closest_halite(self, curr_pos, threshold):
@@ -69,13 +82,28 @@ class Agent():
 		self.coords_2d = int_to_coords(self.coords_1d)
 		return action
 
+	def move_to_target_location(self, target_xy):
+		posistion_xy = self.coords_2d
+		if posistion_xy[0] < target_xy[0]:
+			return 'EAST'
+		if posistion_xy[0] > target_xy[0]:
+			return 'WEST'
+		if posistion_xy[1] < target_xy[1]:
+			return 'SOUTH'
+		if posistion_xy[1] > target_xy[1]:
+			 return 'NORTH'
+		return None
+
 global states
 states = {}
 
 def agent(obs):
+	start = time.time()
 	actions = {}
 	halite, shipyards, ships = obs.players[obs.player]
 	#opp_halite, opp_shipyards, opp_ships = obs.players[1]
+
+	board = HaliteBoard(obs)
 
 	for uid, shipyard in shipyards.items():
 		if(len(ships) == 0):
@@ -88,8 +116,11 @@ def agent(obs):
 
 	for uid, ship_info in ships.items():
 		curr_ship = Agent(ship_info, uid)
+		#print('STATE: ', curr_ship.agent_id, states[curr_ship.agent_id])
 		if(uid not in states):
 			states[uid] = COLLECT
+
+		# collection logic: randomly move around until storage is > 500, at which point path to the closest shipyard
 		if(states[uid] == COLLECT):
 			if(ship_info[1] > 500):
 				states[uid] = DEPOSIT
@@ -98,9 +129,21 @@ def agent(obs):
 				if(action is not None):
 					actions[uid] = action
 
-				#logging.debug(str(halite) + ", " +str(shipyards) + ", "+str(ships))
-				#logging.debug(str(action) + " " + str(halite_coords) + " " + str(ship_coords))
-				#print(action, ship_coords, halite_coords)
+
+		# deposit logic: path naively back to the closest shipyard
+		if(states[uid] == DEPOSIT):
+			closest_shipyard = board.get_closest_shipyard(curr_ship.coords_2d)
+			#print('DEPOSITING to ', curr_ship.coords_2d, closest_shipyard, len(board.get_shipyard_locations()))
+			moves = curr_ship.move_to_target_location(closest_shipyard)
+			if moves == None:
+				ship_action = None
+			else:
+				ship_action = moves
+			if ship_action is not None:
+				actions[uid] = ship_action  
+
 	
+	end = time.time()
+	#print(end-start)
 	return actions
 
